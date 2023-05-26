9f309621535184fbdf83f117fb800c63badc23d1
Rajendra Subedi <rajendrasubedi05@gmail.com>
2023-05-26 19:05:30 +0545

    Updated code with new changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./contact.css"
+import axios from 'axios';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onSubmit = () => {
+    axios.post(`https://sheet.best/api/sheets/f8b17ece-26f3-4600-b92d-815857edc06a`, {
+      name, email, subject, message
+    })
+  }
+
   return (
     <section className="contact container section" id='contact'>
       <h2 className="section__title">Get In Touch</h2>
@@ -15,23 +27,23 @@ const Contact = () => {
         <form action="" className="contact__form">
           <div className="contact__form-group">
             <div className="contact__form-div">
-              <input type="text" className="contact__form-input" placeholder='Insert your name' />
+              <input type="text" className="contact__form-input" onChange={(e)=> setName(e.target.value)} placeholder='Insert your name' />
             </div>
 
             <div className="contact__form-div">
-              <input type="email" className="contact__form-input" placeholder='Insert your email' />
+              <input type="email" className="contact__form-input" onChange={(e)=> setEmail(e.target.value)} placeholder='Insert your email' />
             </div>
           </div>
           
             <div className="contact__form-div">
-              <input type="text" className="contact__form-input" placeholder='Insert your subject' />
+              <input type="text" className="contact__form-input" onChange={(e)=> setSubject(e.target.value)} placeholder='Insert your subject' />
             </div>
 
             <div className="contact__form-div contact__form-area">
-              <textarea name="" id="" cols="30" rows="10" className='contact__form-input' placeholder='Write your message...'></textarea>
+              <textarea name="" id="" cols="30" rows="10" className='contact__form-input' onChange={(e)=> setMessage(e.target.value)} placeholder='Write your message...'></textarea>
             </div>
 
-            <button className="btn">Send Message</button>
+            <button className='btn' type='submit' onClick={onSubmit}>Send Message</button>
         </form>
       </div>
     </section>
